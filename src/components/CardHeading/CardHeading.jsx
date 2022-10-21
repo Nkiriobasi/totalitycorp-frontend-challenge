5e7eb607da225c938a6c075b5f855a9f62c2b4d4
Nkiriobasi <obasinkiri@gmail.com>
2022-10-21 09:27:40 +0100

    created a card heading section

---
+++ src/components/CardHeading/CardHeading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CardHeading.scss';
+
+
+const CardHeading = () => {
+  return (
+    <header>
+        <div className="header__text">
+            <span >{}</span>
+        </div>
+    </header>
+  );
+}
+
+export default CardHeading;
