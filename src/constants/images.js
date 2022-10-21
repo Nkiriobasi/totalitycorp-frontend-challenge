459d07590fbd1897654c37bdce65436b15a2c6ca
Nkiriobasi <obasinkiri@gmail.com>
2022-10-21 09:46:05 +0100

    imported and exported assets to be used in the category section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,27 @@
 import openSea from '../assets/opensea.svg';
 import openSeaWhite from '../assets/opensea-white.svg';
+import artWallpaper from '../assets/art-wallpaper.webp';
+import collectiblesWallpaper from '../assets/collectibles-wallpaper.webp';
+import domainNames from '../assets/domain-names-wallpaper.webp';
+import musicWallpaper from '../assets/music-wallpaper.webp';
+import photographyWallpaper from '../assets/photography-category-wallpaper.webp';
+import sportsWallpaper from '../assets/sports-wallpaper.webp';
+import tradingCardsWallpaper from '../assets/trading-cards-wallpaper.webp';
+import utilityWallpaper from '../assets/utility-wallpaper.webp';
+import virtualWorldsWallpaper from '../assets/virtual-worlds-wallpaper.webp';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     openSea,
-    openSeaWhite
+    openSeaWhite,
+    artWallpaper,
+    collectiblesWallpaper,
+    domainNames,
+    musicWallpaper,
+    photographyWallpaper,
+    sportsWallpaper,
+    tradingCardsWallpaper,
+    utilityWallpaper,
+    virtualWorldsWallpaper
 }
